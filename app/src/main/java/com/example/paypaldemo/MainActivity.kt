308f27e579a59285bc255374eb37381cd504f6cb
morganhah99 <mjh58@njit.edu>
2024-05-24 14:44:25 -0400

    Starting to implement PayPal

--- app/src/main/java/com/example/paypaldemo/MainActivity.kt
+++ app/src/main/java/com/example/paypaldemo/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.paypaldemo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.paypal.android.corepayments.CoreConfig
 import com.paypal.android.corepayments.Environment
 import com.paypal.android.paypalnativepayments.PayPalNativeCheckoutClient
